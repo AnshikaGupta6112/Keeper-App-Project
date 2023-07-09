be15a5adb481e084ca4601668abee57ab88ed7b4
AnshikaGupta6112 <79785110+AnshikaGupta6112@users.noreply.github.com>
2023-07-09 13:39:26 +0530

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles1.css";
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Copyright @ 2023</p>
+    </div>
+  );
+}
+export default Footer;
---
+++ src/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content: "Q. How many programmers does it take to change the frontend"
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content: "How to keep a programmer in the shower forever"
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content: "Q. Why did the programmer quit his job?"
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content: "Q. What's the difference between hardware and software"
+  }
+];
+
+export default notes;
